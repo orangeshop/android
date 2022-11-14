5eed4403146f0c95821a65e64e9ed79269a858c3
choeyeongho <dudgh102416@gmail.com>
2022-11-14 23:54:52 +0900

    111asdasd

--- app/src/main/java/com/example/android/loadding_page.java
+++ app/src/main/java/com/example/android/loadding_page.java
@@ -18,11 +18,6 @@ public class loadding_page extends AppCompatActivity {
 
     int counter =0;
 
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
